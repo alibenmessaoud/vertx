b2cb3d2fba0d381b7e211eedb0a690372aeee196
Ali Ben Messaoud <mail2alibm@gmail.com>
2019-01-16 00:45:12 +0100

    updated model

--- app/src/app/auth0-variables.ts
+++ app/src/app/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
     clientID: 'W1M5bIMw6Pqp5y60h5R6UvSka7eQ6ReP',
     domain: 'alibenmessaoud.auth0.com',
-    callbackURL: 'http://localhost:4200/callback'
+    callbackURL: 'http://localhost:3000/callback'
 };
