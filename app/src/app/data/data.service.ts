253e8f18cc97d270794872dc11bb54a829f2247b
Ali Ben Messaoud <mail2alibm@gmail.com>
2019-01-16 00:30:15 +0100

    update model

--- app/src/app/data/data.service.ts
+++ app/src/app/data/data.service.ts
@@ -1,17 +1,17 @@
 import {Injectable} from '@angular/core';
-import {Post} from '../Post';
+import {Card} from '../Card';
 import {Observable, of} from 'rxjs';
 
 @Injectable()
 export class DataService {
 
-  ELEMENT_DATA: Post[] = [
-    {position: 0, title: 'Post One', category: 'Game 1', date_posted: new Date(), body: 'Body 1'},
-    {position: 1, title: 'Post Two', category: 'Game 3', date_posted: new Date(), body: 'Body 2'},
-    {position: 2, title: 'Post Three', category: 'Game 2', date_posted: new Date(), body: 'Body 3'},
-    {position: 3, title: 'Post Four', category: 'Game 2', date_posted: new Date(), body: 'Body 4'},
-    {position: 4, title: 'Post Five', category: 'Game 4', date_posted: new Date(), body: 'Body 5'},
-    {position: 5, title: 'Post Six', category: 'Game 2', date_posted: new Date(), body: 'Body 6'},
+  ELEMENT_DATA: Card[] = [
+    {position: 0, title: 'Card One', category: 'Game 1', date_posted: new Date(), body: 'Body 1'},
+    {position: 1, title: 'Card Two', category: 'Game 3', date_posted: new Date(), body: 'Body 2'},
+    {position: 2, title: 'Card Three', category: 'Game 2', date_posted: new Date(), body: 'Body 3'},
+    {position: 3, title: 'Card Four', category: 'Game 2', date_posted: new Date(), body: 'Body 4'},
+    {position: 4, title: 'Card Five', category: 'Game 4', date_posted: new Date(), body: 'Body 5'},
+    {position: 5, title: 'Card Six', category: 'Game 2', date_posted: new Date(), body: 'Body 6'},
   ];
   categories = [
     {value: 'Game 1', viewValue: 'Game 1'},
@@ -22,19 +22,19 @@ export class DataService {
   constructor() {
   }
 
-  getData(): Observable<Post[]> {
-    return of<Post[]>(this.ELEMENT_DATA);
+  getData(): Observable<Card[]> {
+    return of<Card[]>(this.ELEMENT_DATA);
   }
 
   getCategories() {
     return this.categories;
   }
 
-  addPost(data) {
+  add(data) {
     this.ELEMENT_DATA.push(data);
   }
 
-  deletePost(index) {
+  delete(index) {
     this.ELEMENT_DATA = [...this.ELEMENT_DATA.slice(0, index), ...this.ELEMENT_DATA.slice(index + 1)];
   }
 
