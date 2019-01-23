c2edd8251b342bc28694733a38408d167c049db8
Ali Ben Messaoud <mail2alibm@gmail.com>
2019-01-23 01:30:10 +0100

    card page

--- app/src/app/card/card.component.ts
+++ app/src/app/card/card.component.ts
@@ -11,15 +11,19 @@ import { DataService } from '../data/data.service';
 export class CardComponent implements OnInit, OnDestroy {
 
   card: Card;
+  url: string;
+
   routeSubscription: any;
   dataServiceSubscription: any;
 
-  constructor(private route: ActivatedRoute, private dataService: DataService) { }
+  constructor(private route: ActivatedRoute, private dataService: DataService) {
+    this.url = window.location.href
+  }
 
   ngOnInit() {
     this.routeSubscription = this.route.params.subscribe(params => {
       const code = params['code']; // (+) converts string 'id' to a number
-      this.dataServiceSubscription = this.dataService.get(code).subscribe(response => {this.card = response});
+      this.dataServiceSubscription = this.dataService.get(code).subscribe(response => {this.card = response; console.log(response)});
     });
   }
 
